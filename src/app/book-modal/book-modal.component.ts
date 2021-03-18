dd8f9852aaa3b3d25e34f2ce45388b0522f94a7a
Ramiro Trejo <trejoramiro1@gmail.com>
2021-03-18 13:57:55 -0500

    fix modal text rendering

--- src/app/book-modal/book-modal.component.ts
+++ src/app/book-modal/book-modal.component.ts
@@ -10,18 +10,39 @@ import { BookService } from './../book.service';
 export class BookModalComponent implements OnInit {
 
   @Input() book: any;
-  bookInformation: any;
+  bookInformation = {
+    details: {
+      genres: '',
+      copyright_date: '',
+      description: ''
+    }
+  };
 
   constructor(public activeModal: NgbActiveModal, private bookService: BookService) { }
 
   ngOnInit(): void {
     this.bookService.getBookInformation(this.book.ISBN).subscribe({ 
       next: data => {
-        console.log('isbn', this.book.ISBN);
-        this.bookInformation = data[`ISBN:0${this.book.ISBN}`];
-        console.log('data', data);
+        if (Object.keys(data).length !== 0) {
+          console.log('isbn', this.book.ISBN);
+          this.bookInformation = data[`ISBN:0${this.book.ISBN}`];
+          console.log('data', data);
+        }
       }
     });
   }
 
+  showBookDescription(): string {
+    if (typeof this.bookInformation["details"]["description"] === 'object' && this.bookInformation["details"]["description"] !== null) {
+      return this.bookInformation["details"]["description"]["value"];
+    }
+    return this.bookInformation["details"]["description"];
+  }
+
+  showGenre(): string {
+    if (this.bookInformation["details"]["genres"]) {
+      return `Genre: ${this.bookInformation["details"]["genres"]}`;
+    }
+    return '';
+  }
 }
